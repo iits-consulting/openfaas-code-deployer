cb2899c75e6f3089fdbc9d42aba8ed96b9c9e8a0
Alexis Hildebrandt <Alexis.Hildebrandt@wirksames-design.de>
2021-08-12 15:59:40 +0200

    slack middleware

--- slack-middleware/handler.js
+++ slack-middleware/handler.js
@@ -1,17 +1,83 @@
 'use strict'
 
+const _ = require('lodash')
+const axios = require('axios')
+
+const config = {
+  faas: {
+    baseUrl: 'https://dfd-openfaas.iits.tech'
+  }
+}
+
+const commands = {
+  DEPLOY: 'DEPLOY',
+  RUN: 'RUN',
+}
+
+const languages = {
+  JAVASCRIPT: 'JAVASCRIPT',
+}
+
 module.exports = async (event, context) => {
-  const input = event.body.text.split(' ')
-
-  const command = input.shift()
-  const payload = input.join(' ')
-
+  let input
   let message
-  if (command === 'deploy') {
-    message = `deploying ${payload} :partyparrot:`
+
+  try {
+    const command = determineCommand(input)
+    if (command === commands.DEPLOY) {
+      const language = determineLanguage(input)
+      const payload = determinePayload(input)
+      message = `deploying your ${language} function ${payload} :partyparrot:`
+    } else if (command === commands.RUN) {
+      const functionName = determineFunctionName(input)
+      const payload = determinePayload(input)
+      message = `running ${functionName} with payload ${payload} :partyparrot:`
+      const {data} = await axios.post(`${config.faas.baseUrl}/function/`, payload)
+
+      message += "\n" + JSON.stringify(data)
+
+    } else {
+      message = `unknown command ${command}`
+    }
+  } catch (error) {
+    message = error.message
   }
 
   return context
     .status(200)
     .succeed(message)
+}
+
+
+const determineCommand = (input) => {
+  const command = input.split(' ').shift().toUpperCase()
+
+  if (Object.keys(commands).includes(command) === false) {
+    throw new Error(`unknown command ${command}`)
+  }
+
+  return command
+}
+
+const determineLanguage = (input) => {
+  const language = input.split(' ')[1].toUpperCase()
+
+  if (Object.keys(languages).includes(language) === false) {
+    throw new Error(`unknown language ${language}`)
+  }
+
+  return language
+}
+
+const determineFunctionName = (input) => {
+  return input.split(' ')[1]
+}
+
+const determinePayload = (input) => {
+  return input.split(' ')[2]
+}
+
+const deploy = (language, code) => {
+}
+const run = (functionName, args) => {
 }
